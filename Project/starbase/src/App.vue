fc1f9898cab52e804163a90a37b5e07925a0479d
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-12 21:12:45 +0000

    created main.js and app.vue

---
+++ Project/starbase/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id= "app">
+        {{title}}
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'app',
+    data(){
+        return {
+            title: 'Generate Your team'
+        }
+    }
+}
+</script>
