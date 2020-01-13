4ba1522372bd9efd46fabb190a9113684f9a1056
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-01-13 23:50:40 +0000

    added vue bootstrap

--- Project/starbase/src/App.vue
+++ Project/starbase/src/App.vue
@@ -2,7 +2,7 @@
     <div id ="app">
        <h3> {{msg}} </h3>
       <h6> {{title}}</h6>
-
+    <button btn btn-primary @click= "fetchCharacter" >Get Characters</button>
     </div>
 </template>
 <script>
@@ -12,6 +12,14 @@ export default {
         return {
             title: 'Generate Your team here now ',
             msg:'WELCOME TO THE NEXT '
+        }
+    }, 
+    methods: {
+        fetchCharacter(){
+            fetch('https://swapi.co/api/people', {
+                method: `GET`
+            })
+            .then(response => console.log(response))
         }
     }
 }
